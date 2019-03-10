68e90362863b75a9695d378f8690c3743c6ab5db
Kota Watanabe <kota3316@gmail.com>
2019-03-09 20:42:21 -0800

    Add boxIt function but still need to modify const wordInBox

--- BoxItScipt/boxofword.js
+++ BoxItScipt/boxofword.js
@@ -42,21 +42,58 @@ const box_list_of_words ={
     drawBarsAround: function drawBarsAround(string){
         let bar = "|";
         let wordWithBar = bar + string + bar;
-        
-        return wordWithBar
+        return wordWithBar;
     },
 
+    boxIt: function boxIt(arr){
+        // first, count max words number 
+        let maxWordLength = arr[0].length;
+        for(let word of arr){
+            if(maxWordLength < word.length){
+                maxWordLength = word.length;
+            }
+         }
+        let boxTop = this.drawTopBorder(maxWordLength);    
+        let boxMiddle = this.drawMiddleBorder(maxWordLength);
+        let boxBottom = this.drawBottomBorder(maxWordLength);
+        let stringWithSpace = ""; 
+        let spaceFromMaxLength = 0;
+
+        for(let word of arr){
+            stringWithSpace = word
+        }
+
+        for(let string of arr){
+            spaceFromMaxLength =  maxWordLength - string.length;
+        }
+
+        for(let i = 0; i < spaceFromMaxLength;i++){
+            stringWithSpace += " "
+        }
+        let barWord = this.drawBarsAround(stringWithSpace);
+         
+
+        let wordInBox = `${boxTop}\n${barWord}\n${boxMiddle}\n${boxBottom}`;
+        return wordInBox;
+        },
 
 
-}
+        
+        
+    }
+
+
+
+
 
 let a = 4;
 let b = 1;
 let c = 8;
 
-console.log(box_list_of_words.drawline(a));
-console.log(box_list_of_words.drawTopBorder(c));
-console.log(box_list_of_words.drawMiddleBorder(c));
-console.log(box_list_of_words.drawBottomBorder(c));
-console.log(box_list_of_words.drawBarsAround("my name is Dan"));
-console.log(box_list_of_words.drawBarsAround("   You are Bill"));
+// console.log(box_list_of_words.drawline(a));
+// console.log(box_list_of_words.drawTopBorder(c));
+// console.log(box_list_of_words.drawMiddleBorder(c));
+// console.log(box_list_of_words.drawBottomBorder(c));
+// console.log(box_list_of_words.drawBarsAround("my name is Dan"));
+// console.log(box_list_of_words.drawBarsAround("   You are Bill"));
+console.log(box_list_of_words.boxIt(['Jon Snow', 'Cersei Lannister']));
